8921d2969490b255773c7fb5a6d754da23a68d68
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2021-10-03 18:05:23 -0300

    gradients

--- SwiftfulThinkingBootcamp/GradientsBootcamp.swift
+++ SwiftfulThinkingBootcamp/GradientsBootcamp.swift
@@ -10,7 +10,24 @@ import SwiftUI
 struct GradientsBootcamp: View {
     var body: some View {
         RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-            .fill(Color.red)
+            .fill(
+//                Color.red
+//                LinearGradient(
+//                    gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1))]),
+//                    startPoint: .topLeading,
+//                    endPoint: .bottom)
+                
+//                RadialGradient(
+//                    gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1))]),
+//                    center: .leading,
+//                    startRadius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/,
+//                    endRadius: 200)
+                
+                AngularGradient(
+                    gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1))]),
+                    center: .topLeading,
+                    angle: .degrees(180 + 45))
+            )
             .frame(width: 300, height: 200)
     }
 }
